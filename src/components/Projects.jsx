ccfaba3d462f48eeb3ac2f277877fb8720aff3e1
JaiAakash2005 <jaiaakash57@gmail.com>
2024-06-12 12:23:57 +0530

    Brief description of the changes made

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,79 +1,11 @@
-// import React from "react";
-// import "./Projects.css";
-// import OIP from "../assets/OIP.jpeg";
-// import OIP2 from "../assets/Designer.png";
-// import OIP3 from "../assets/OIP (1).jpeg";
-
-// const Projects = () => {
-//   return (
-//     <div className="project">
-//       <h1 style={{ overflow: "hidden", padding: "5px", marginTop: "10px" }}>
-//         Latest Project
-//       </h1>
-//       <div className="row project-container">
-//         <div className="col-lg-4 col-12">
-//           <div className="image-wrapper">
-//             <img src={OIP} alt="Project 1" className="project-img" />
-//             <div className="overlay">
-//               <div className="arrow">
-//                 <a
-//                   href=""
-//                   type="button"
-//                   style={{ textDecoration: "none" }}
-//                   className="arrow-link"
-//                 >
-//                   &#8594;
-//                 </a>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//         <div className="col-lg-4 col-12">
-//           <div className="image-wrapper">
-//             <img src={OIP2} alt="Project 2" className="img-stem" />
-//             <div className="overlay">
-//               <div className="arrow">
-//                 <a
-//                   href=""
-//                   type="button"
-//                   style={{ textDecoration: "none" }}
-//                   className="arrow-link"
-//                 >
-//                   &#8594;
-//                 </a>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//         <div className="col-lg-4 col-12">
-//           <div className="image-wrapper">
-//             <img src={OIP3} alt="Project 3" className="project3" />
-//             <div className="overlay">
-//               <div className="arrow">
-//                 <a
-//                   href=""
-//                   type="button"
-//                   style={{ textDecoration: "none" }}
-//                   className="arrow-link"
-//                 >
-//                   &#8594;
-//                 </a>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Projects;
-
 import React, { useEffect } from "react";
 import "./Projects.css";
 import OIP from "../assets/OIP.jpeg";
-import OIP2 from "../assets/Designer.png";
+import R from "../assets/R.jpeg";
 import OIP3 from "../assets/OIP (1).jpeg";
+import port from "../assets/port.jpeg";
+import todo from "../assets/todolist.jpg";
+import calc from "../assets/calc.jpeg";
 
 const Projects = () => {
   useEffect(() => {
@@ -96,7 +28,7 @@ const Projects = () => {
       <div className="row project-container">
         <div className="col-lg-4 col-12">
           <div className="image-wrapper">
-            <img src={OIP} alt="Project 1" className="project-img" />
+            <img src={OIP} alt="Project 1" className="project-img img-fluid" />
             <div className="overlay">
               <div className="content">
                 <a
@@ -112,7 +44,7 @@ const Projects = () => {
         </div>
         <div className="col-lg-4 col-12">
           <div className="image-wrapper">
-            <img src={OIP2} alt="Project 2" className="img-stem" />
+            <img src={R} alt="Project 2" className="img-stem img-fluid" />
             <div className="overlay">
               <div className="content">
                 <a
@@ -127,12 +59,66 @@ const Projects = () => {
           </div>
         </div>
         <div className="col-lg-4 col-12">
-          <div className="image-wrapper">
-            <img src={OIP3} alt="Project 3" className="project3" />
+          <div className="image-wrapper img-wrap">
+            <img src={OIP3} alt="Project 3" className="project3 img-fluid" />
             <div className="overlay">
               <div className="content">
                 <a
                   href="https://github.com/JaiAakash2005/Travel"
+                  type="button"
+                  className="arrow-link"
+                >
+                  &#8594;
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row project-container">
+        <div className="col-lg-4 col-12">
+          <div className="image-wrapper">
+            <img src={port} alt="Project 1" className="project-img img-fluid" />
+            <div className="overlay">
+              <div className="content">
+                <a
+                  href="https://github.com/JaiAakash2005/Portfolio-react-1"
+                  type="button"
+                  className="arrow-link"
+                >
+                  &#8594;
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-4 col-12">
+          <div className="image-wrapper">
+            <img src={todo} alt="Project 2" className="img-stem img-fluid" />
+            <div className="overlay">
+              <div className="content">
+                <a
+                  href="https://github.com/JaiAakash2005/Todo"
+                  type="button"
+                  className="arrow-link"
+                >
+                  &#8594;
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-4 col-12">
+          <div className="image-wrapper img4">
+            <img
+              src={calc}
+              alt="Project 3"
+              className="project3 calc img-fluid"
+            />
+            <div className="overlay">
+              <div className="content">
+                <a
+                  href="https://github.com/JaiAakash2005/calculator"
                   type="button"
                   className="arrow-link"
                 >
